2e73899a69c4c706609278168bc49e7ec1227420
Franz Detro <franz.detro@native-instruments.de>
2014-02-26 12:23:34 +0100

    improved detection of ARM platform (check for Apple clang __arm64); set version to 8.0.0 in case of __arm64 (allows check for armv8 targets)

--- include/boost/predef/architecture/arm.h
+++ include/boost/predef/architecture/arm.h
@@ -20,10 +20,12 @@ http://www.boost.org/LICENSE_1_0.txt)
     [[__predef_symbol__] [__predef_version__]]
 
     [[`__arm__`] [__predef_detection__]]
+    [[`__arm64`] [__predef_detection__]]
     [[`__thumb__`] [__predef_detection__]]
     [[`__TARGET_ARCH_ARM`] [__predef_detection__]]
     [[`__TARGET_ARCH_THUMB`] [__predef_detection__]]
 
+    [[`__arm64`] [8.0.0]]
     [[`__TARGET_ARCH_ARM`] [V.0.0]]
     [[`__TARGET_ARCH_THUMB`] [V.0.0]]
     ]
@@ -31,9 +33,12 @@ http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_ARCH_ARM BOOST_VERSION_NUMBER_NOT_AVAILABLE
 
-#if defined(__arm__) || defined(__thumb__) || \
+#if defined(__arm__) || defined(__arm64) || defined(__thumb__) || \
     defined(__TARGET_ARCH_ARM) || defined(__TARGET_ARCH_THUMB)
 #   undef BOOST_ARCH_ARM
+#   if !defined(BOOST_ARCH_ARM) && defined(__arm64)
+#       define BOOST_ARCH_ARM BOOST_VERSION_NUMBER(8,0,0)
+#   endif
 #   if !defined(BOOST_ARCH_ARM) && defined(__TARGET_ARCH_ARM)
 #       define BOOST_ARCH_ARM BOOST_VERSION_NUMBER(__TARGET_ARCH_ARM,0,0)
 #   endif
