9e5f79735cd65cb429cf2f6c2d16cb74d14ad185
Franz Detro <franz.detro@native-instruments.de>
2014-03-12 14:09:22 +0100

    added missing 'Copyright'

--- include/boost/predef/os/macos.h
+++ include/boost/predef/os/macos.h
@@ -1,6 +1,6 @@
 /*
 Copyright Rene Rivera 2008-2013
-Franz Detro 2014
+Copyright Franz Detro 2014
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE_1_0.txt or copy at
 http://www.boost.org/LICENSE_1_0.txt)
