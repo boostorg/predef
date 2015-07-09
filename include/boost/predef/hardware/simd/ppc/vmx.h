29a0d1155303410a6dd8c3d8a9d7afe7434ae0c8
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-09 10:01:56 +0200

    PPC: Add VMX predef

--- include/boost/predef/hardware/simd/ppc/vmx.h
+++ include/boost/predef/hardware/simd/ppc/vmx.h
@@ -9,4 +9,38 @@ http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_PREDEF_HARDWARE_SIMD_PPC_VMX_H
 #define BOOST_PREDEF_HARDWARE_SIMD_PPC_VMX_H
 
+#include <boost/predef/version_number.h>
+
+/*`
+ [heading `BOOST_HW_SIMD_PPC_VMX`]
+
+ [@https://en.wikipedia.org/wiki/AltiVec#VMX128 VMX] powerpc extension.
+ Version number is: 1.0.0.
+
+ [table
+     [[__predef_symbol__] [__predef_version__]]
+
+     [[`__ALTIVEC__`] [__predef_detection__]]
+     [[`__VEC__`] [__predef_detection__]]
+     ]
+ */
+
+#define BOOST_HW_SIMD_PPC_VMX BOOST_VERSION_NUMBER_NOT_AVAILABLE
+
+#if defined(__ALTIVEC__) || defined(__VEC__)
+#   undef BOOST_HW_SIMD_PPC_VMX
+#   define BOOST_HW_SIMD_PPC_VMX BOOST_VERSION_NUMBER(1, 0, 0)
+#   define BOOST_HW_SIMD_PPC_VMX_AVAILABLE
 #endif
+
+#define BOOST_HW_SIMD_PPC_VMX_NAME "VMX"
+
+#if !defined(BOOST_HW_SIMD_PPC) && defined(BOOST_HW_SIMD_PPC_VMX_AVAILABLE)
+#   define BOOST_HW_SIMD_PPC BOOST_HW_SIMD_PPC_VMX
+#   define BOOST_HW_SIMD_PPC_NAME BOOST_HW_SIMD_PPC_VMX_NAME
+#endif
+
+#endif
+
+#include <boost/predef/detail/test.h>
+BOOST_PREDEF_DECLARE_TEST(BOOST_HW_SIMD_PPC_VMX, BOOST_HW_SIMD_PPC_VMX_NAME)
