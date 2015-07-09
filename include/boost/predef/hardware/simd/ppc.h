9b66cddaf69b716353b48a779b536a07f2e570e1
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-09 16:12:02 +0200

    Add doc + _AVAILABLE for simd/ppc.h

--- include/boost/predef/hardware/simd/ppc.h
+++ include/boost/predef/hardware/simd/ppc.h
@@ -9,9 +9,30 @@ http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_PREDEF_HARDWARE_SIMD_PPC_H
 #define BOOST_PREDEF_HARDWARE_SIMD_PPC_H
 
-// from the newest to the oldest
+/*`
+ [heading `BOOST_HW_SIMD_PPC`]
+
+ The SIMD extension for x86 (if detected).
+ Version number depends on the detected extension.
+
+ BOOST_HW_SIMD_PPC is defined by the first detected extension. They are
+ checked from the newest to the oldest. (QPX -> VSX -> VMX)
+
+ [table
+     [[__predef_symbol__] [__predef_version__]]
+
+     [[`BOOST_HW_SIMD_PPC`] [__predef_detection__]]
+     [[`BOOST_HW_SIMD_PPC`] [V.R.P]]
+     ]
+ */
+
+// From the newest to the oldest
 #include <boost/predef/hardware/simd/ppc/qpx.h>
 #include <boost/predef/hardware/simd/ppc/vsx.h>
 #include <boost/predef/hardware/simd/ppc/vmx.h>
+
+#if defined(BOOST_HW_SIMD_PPC)
+#   define BOOST_HW_SIMD_PPC_AVAILABLE
+#endif
 
 #endif
