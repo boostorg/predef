f0a0af764c0fca31800b5b4ae8cd5a524c504b61
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-09 16:11:43 +0200

    Add doc + _AVAILABLE for simd/arm.h

--- include/boost/predef/hardware/simd/arm.h
+++ include/boost/predef/hardware/simd/arm.h
@@ -9,6 +9,26 @@ http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_PREDEF_HARDWARE_SIMD_ARM_H
 #define BOOST_PREDEF_HARDWARE_SIMD_ARM_H
 
+/*`
+ [heading `BOOST_HW_SIMD_ARM`]
+
+ The SIMD extension for ARM (if detected).
+ Version number depends on the detected extension.
+
+ BOOST_HW_SIMD_ARM is defined by the first detected extension.
+
+ [table
+     [[__predef_symbol__] [__predef_version__]]
+
+     [[`BOOST_HW_SIMD_ARM`] [__predef_detection__]]
+     [[`BOOST_HW_SIMD_ARM`] [V.R.P]]
+     ]
+ */
+
 #include <boost/predef/hardware/simd/arm/neon.h>
+
+#if defined(BOOST_HW_SIMD_ARM)
+#   define BOOST_HW_SIMD_ARM_AVAILABLE
+#endif
 
 #endif
