7399d0169dd969c62ca74a917971b2c6a7115dad
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-20 11:03:05 +0200

    Refactor ARM SIMD detection + Add _VERSION(s) for ARM family

--- include/boost/predef/hardware/simd/arm.h
+++ include/boost/predef/hardware/simd/arm.h
@@ -6,36 +6,47 @@ Distributed under the Boost Software License, Version 1.0.
 http://www.boost.org/LICENSE_1_0.txt)
 */
 
-// From the newest to the oldest
-#include <boost/predef/hardware/simd/arm/neon.h>
-
 #ifndef BOOST_PREDEF_HARDWARE_SIMD_ARM_H
 #define BOOST_PREDEF_HARDWARE_SIMD_ARM_H
 
 #include <boost/predef/version_number.h>
+#include <boost/predef/hardware/simd/arm/versions.h>
 
 /*`
  [heading `BOOST_HW_SIMD_ARM`]
 
  The SIMD extension for ARM (if detected).
- Version number depends on the detected extension.
-
- BOOST_HW_SIMD_ARM is defined by the first detected extension.
+ Version number depends on the most recent detected extension.
 
  [table
      [[__predef_symbol__] [__predef_version__]]
 
-     [[`BOOST_HW_SIMD_ARM`] [__predef_detection__]]
-     [[`BOOST_HW_SIMD_ARM`] [V.R.P]]
+
+     [[__predef_detection__] [__predef_detection__]]
+     [[`__aarch64__`] [__predef_detection__]]
+     [[`_M_ARM`] [__predef_detection__]]
+
+
+     [[`__ARM_NEON__`] [BOOST_HW_SIMD_ARM_NEON_VERSION]]
+     [[`__aarch64__`] [BOOST_HW_SIMD_ARM_NEON_VERSION]]
+     [[`_M_ARM`] [BOOST_HW_SIMD_ARM_NEON_VERSION]]
      ]
  */
 
-#if defined(BOOST_HW_SIMD_ARM)
-#   define BOOST_HW_SIMD_ARM_AVAILABLE
-#else
-#   define BOOST_HW_SIMD_ARM BOOST_VERSION_NUMBER_NOT_AVAILABLE
-#   define BOOST_HW_SIMD_ARM_NAME "ARM SIMD"
+#define BOOST_HW_SIMD_ARM BOOST_VERSION_NUMBER_NOT_AVAILABLE
+
+#undef BOOST_HW_SIMD_ARM
+#if !defined(BOOST_HW_SIMD_ARM) && (defined(__ARM_NEON__) || defined(__aarch64__) || defined (_M_ARM))
+#   define BOOST_HW_SIMD_ARM BOOST_HW_SIMD_ARM_NEON_VERSION
 #endif
+
+#if !defined(BOOST_HW_SIMD_ARM)
+#   define BOOST_HW_SIMD_ARM BOOST_VERSION_NUMBER_NOT_AVAILABLE
+#else
+#   define BOOST_HW_SIMD_ARM_AVAILABLE
+#endif
+
+#define BOOST_HW_SIMD_ARM_NAME "ARM SIMD"
 
 #endif
 
