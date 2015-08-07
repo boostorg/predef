bfac404a0f667a9294d19e1151852009bf157ce4
Rene Rivera <grafikrobot@gmail.com>
2015-08-06 20:51:56 -0500

    Bump to version 1.4 for next release. Switched BBv2 check support to use the compile only check utility to address cross-compile use cases.

--- check/predef_check_cc.h
+++ check/predef_check_cc.h
@@ -4,12 +4,16 @@ Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE_1_0.txt or copy at
 http://www.boost.org/LICENSE_1_0.txt)
 */
+#include <boost/predef.h>
 
-#ifndef BOOST_PREDEF_VERSION_H
-#define BOOST_PREDEF_VERSION_H
-
-#include <boost/predef/version_number.h>
-
-#define BOOST_PREDEF_VERSION BOOST_VERSION_NUMBER(1,4,0)
-
+#ifdef CHECK
+#   if ((CHECK) == 0)
+#       error "FAILED"
+#   endif
 #endif
+
+int dummy()
+{
+	static int d = 0;
+	return d++;
+}
