c4691ad11eef332a264a7025979da6e8e8b3dcc2
James E. King III <jking@apache.org>
2017-11-09 03:41:41 -0500

    rename library/c/cloudlibc.h to library/c/cloudabi.h fix typo in cloudlibc detection

--- include/boost/predef/library/c/cloudabi.h
+++ include/boost/predef/library/c/cloudabi.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (C) 2017 James E. King, III
+ * Copyright (C) 2017 James E. King III
  *
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  *   http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#ifndef BOOST_PREDEF_LIBRARY_C_CLOUDLIBC_H
-#define BOOST_PREDEF_LIBRARY_C_CLOUDLIBC_H
+#ifndef BOOST_PREDEF_LIBRARY_C_CLOUDABI_H
+#define BOOST_PREDEF_LIBRARY_C_CLOUDABI_H
 
 #include <boost/predef/version_number.h>
 #include <boost/predef/make.h>
@@ -37,7 +37,7 @@ Version number available as major, and minor.
 
 #if defined(__cloudlibc__)
 #   undef BOOST_LIB_C_CLOUDABI
-#   define BOOST_LIB_C_CLOUBABI \
+#   define BOOST_LIB_C_CLOUDABI \
             BOOST_VERSION_NUMBER(__cloudlibc_major__,__cloudlibc_minor__,0)
 #endif
 
