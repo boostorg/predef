d33798214da9cde1dc3274eb8d804b212e0af7ca
Rene Rivera <grafikrobot@gmail.com>
2019-01-03 21:57:47 -0600

    Cleanups of cmake build use support.

--- test/test_cmake/main.cpp
+++ test/test_cmake/main.cpp
@@ -1,7 +1,19 @@
-// Dummy executable, just to make sure
-// that we can find the Boost.predef header files
+/*
+Copyright Mike Dev 2018
+Copyright 2018 Rene Rivera
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt
+*/
+
+/*
+Dummy executable, just to make sure that we can find the Boost.predef header
+files.
+*/
+
 #include <boost/predef.h>
 
-int main() {
-
+int main()
+{
+    return 0;
 }
